366d263f1522cca96029c28f533b0b3909f4a1d3
Morning1139Angel <rubick1139@gmail.com>
2023-04-18 11:55:54 +0330

    added code for playing with arrays

--- go-syntax/main.go
+++ go-syntax/main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//🌼 Arrays
+	//=================================================
+	intArr32 := [32]int{} //The type of elements and length are
+
+	//the last elem will be the zero value
+	firstIntArr5 := [5]int{1, 3, 5, 4} // both part of the array type
+
+	fmt.Printf("intArr32: %v\n", intArr32)
+	fmt.Printf("firstIntArr5: %v\n", firstIntArr5)
+
+	//ERROR!
+	//cannot use firstIntArr5 (variable of type [5]int) as [32]int value in assignment
+	// intArr32 = firstIntArr5
+
+	//ERROR!
+	// cannot convert firstIntArr5 (variable of type [5]int) to type [32]int
+	//intArr32 = [32]int(firstIntArr5)
+
+	//zeor value
+	var secondIntArr5 [5]int
+	fmt.Printf("before assignment secondIntArr5: %v\n", secondIntArr5)
+
+	secondIntArr5 = firstIntArr5 //valid assignment... both same type
+	fmt.Printf("after assignment secondIntArr5: %v\n", secondIntArr5)
+
+	//2d array
+	var twoD [2][3]int
+	fmt.Println("count of 2d array rows:", len(twoD))
+	fmt.Println("count of 2d array rows:", len(twoD))
+
+	for i := 0; i < len(twoD); i++ {
+		twoD[i] = [3]int{i + 1, i + 2, i + 3}
+
+	}
+	fmt.Println("lenght of 2d array :", len(twoD))
+
+	for rowIndx, row := range twoD {
+		fmt.Println("============================")
+		fmt.Printf("Im iterating over the \"twoD\" array's row number %d\n", rowIndx)
+		for columnIndx, value := range row {
+			fmt.Printf("element at index [%d][%d] is: %d\n", rowIndx, columnIndx, value)
+		}
+	}
+	//=================================================
 }
